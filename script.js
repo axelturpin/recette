4378ab3eee1e1a2af7fac8bc69f0dda6f7a3004f
axelturpin <axelturpin@hotmail.com>
2026-02-09 15:46:05 +0100

    t

---
+++ script.js
@@ -0,0 +1,12 @@
+// Gestionnaire
+// de recettes de cuisine
+// Ajouter
+//     / modifier / supprimer des recettes
+// Catégoriser
+//     par type (entrée, plat, dessert)
+// Filtrer
+//     par catégorie ou ingrédients
+// Sauvegarde
+//     en LocalStorage
+// Recettes
+//     favorites
