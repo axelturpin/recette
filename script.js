709bcea55923953fc202a68fd624e181bee02041
axelturpin <axelturpin@hotmail.com>
2026-02-10 15:49:37 +0100

    marche pas

--- script.js
+++ script.js
@@ -13,10 +13,31 @@
 
 const btn = document.querySelector(".btn_ajout");
 btn.addEventListener("click", (e)=> {
-    e.preventDefault()
-    const nom = document.querySelector(".nom").value;
-    const type = document.querySelector(".type").value;
-    const description = document.querySelector(".description").value;
-    const ingrédients = document.querySelector(".ingrédients").value;
-    console.log(nom);
+    e.preventDefault();
+    let recettes = document.createElement("div");
+    if (localStorage.getItem("recettes")){
+        recettes = JSON.parse(localStorage.getItem("recettes"));
+    }
+
+    const nom = document.querySelector("#nom").value;
+    const type = document.querySelector("#type").value;
+    const description = document.querySelector("#description").value;
+    const ingrédients = document.querySelector("#ingrédients").value;
+
+    const divNom = document.createElement("p").textContent = nom;
+    const divType = document.createElement("p").textContent = type;
+    const divDescription = document.createElement("p").textContent = description;
+    const divIngrédients = document.createElement("p").textContent = ingrédients;
+    // console.log(divDescription);
+
+    const recette = document.createElement("div");
+    recette.append(divNom, divType, divDescription, divIngrédients);
+    // console.log(recette);
+
+    recettes.appendChild(recette);
+    // console.log(recettes);
+    localStorage.setItem("recettes", JSON.stringify(recettes));
+
+    const liste = document.querySelector(".liste");
+    liste.appendChild(recettes);
 })
